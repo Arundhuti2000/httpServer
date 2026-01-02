4665985cb0bed510dbe6dd12b0bae7f341de8da0
Arundhati Das <arundhutidas2000@gmail.com>
2026-01-02 09:16:16 -0500

    Implement user management with PostgreSQL: add user creation functionality and database schema

--- main.go
+++ main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Arundhuti2000/httpserver/internal/database"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB *database.Queries
 }
 
 
+
 func main(){
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err !=nil{
+		fmt.Sprintf(err.Error())
+	}
+	dbQueries := database.New(db)
 	const filepathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
 	cfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
+		DB: dbQueries,
 	}
 	fileserverhandler:=cfg.middlewareMetricsInc(http.StripPrefix("/app/",http.FileServer(http.Dir(filepathRoot))))
 	mux.Handle("/app/", fileserverhandler)
